d1d6d57771824a2f300c8487c2cf5a0984054490
Yibin Z <zhyib002@outlook.com>
2024-01-14 23:28:11 +0800

    update

---
+++ src/Week/Week379/100170.ts
@@ -0,0 +1,24 @@
+function areaOfMaxDiagonal(dimensions: number[][]): number {
+  let area = 0;
+  let max = 0;
+  for (let i = 0; i < dimensions.length; i++) {
+    const len = Math.sqrt(
+      dimensions[i][0] * dimensions[i][0] + dimensions[i][1] * dimensions[i][1]
+    );
+    if (
+      len > max ||
+      (len == max && dimensions[i][0] * dimensions[i][1] > area)
+    ) {
+      max = len;
+      area = dimensions[i][0] * dimensions[i][1];
+    }
+  }
+  return area;
+}
+
+areaOfMaxDiagonal([
+  [2, 6],
+  [5, 1],
+  [3, 10],
+  [8, 4],
+]);
